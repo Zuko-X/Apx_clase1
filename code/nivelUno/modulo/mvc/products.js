cca7106a96ad4c2a6c8dd6c2a8659d3949c8c441
Skaysz <thomas680159753@gmail.com>
2023-04-25 10:36:36 -0300

    -Cambios en productos

--- code/nivelUno/modulo/mvc/products.js
+++ code/nivelUno/modulo/mvc/products.js
@@ -1,22 +1,22 @@
-const array = [
+const productos = [
     {
         id: 1,
         nombre: "producto 1",
-        stock: 3,
-    }
+        stock: 27,
+    },
     {
-        id: 1,
-        nombre: "producto 1",
-        stock: 3,
-    }
+        id: 2,
+        nombre: "producto 2",
+        stock: 14,
+    },
     {
-        id: 1,
-        nombre: "producto 1",
-        stock: 3,
-    }
+        id: 3,
+        nombre: "producto 3",
+        stock: 32,
+    },
     {
-        id: 1,
-        nombre: "producto 1",
-        stock: 3,
-    }
+        id: 4,
+        nombre: "producto 4",
+        stock: 46,
+    },
 ];
