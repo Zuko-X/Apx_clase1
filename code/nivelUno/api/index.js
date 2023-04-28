67bb331dd3d097da0f5ef0ce9a83c02256e49556
MorePC <thomas680159753@gmail.com>
2023-04-27 22:43:41 -0300

    Fix index.js

--- code/nivelUno/api/index.js
+++ code/nivelUno/api/index.js
@@ -1,23 +1,49 @@
 const { argv } = require("node:process")
 
-const [nodeExecPath, executedFilePath, ...args] = argv
+// const [nodeExecPath, executedFilePath, ...args] = argv
+
+// Definir los objetos con valores nulos
 const objetos = {
     nombre: null,
     apellido: null,
     edad: null,
 }
 
-const nose = () => {
-    objetos.nombre = args.shift();
-    console.log(objetos.nombre)
+const takeArguments = () => {
+    // Analizar los argumentos
+    for (let i = 0; i < args.length; i += 2) {
+        // Si el argumento no tiene un valor, salir del loop
+        if (i + 1 >= args.length) {
+            break;
+        }
+        // Obtener el nombre y el valor del argumento
+        const name = args[i].replace(/^--/, "");
+        const value = args[i + 1];
+        
+        // Asignar el valor al objeto
+        switch (name) {
+            case "nombre":
+                objetos.nombre = value;
+                break;
+            case "apellido":
+                objetos.apellido = value;
+                break;
+            case "edad":
+                objetos.edad = value;
+                break;
+            default:
+                console.error(`El argumento "${name}" no es válido`);
+                process.exit(1);
+        }
+    }
 }
 
+// Obtener los argumentos de la línea de comandos
+let args = process.argv.slice(2);
 
 function main() {
-    nose();
-    console.log(args)
+    takeArguments();
+    console.log(objetos);
 }
 
 main();
-
-objetos
